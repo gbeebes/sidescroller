e2b3d80a0795da32829008c92895faaf1d1dd4bb
Gillian Beebe <gillian@Mac.lan>
2025-03-06 18:57:28 -0500

    first steps

---
+++ main.js
@@ -0,0 +1,14 @@
+class Game{
+    constructor(canvas, context){
+        this.canvas = canvas;
+        this.context = context;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+    }
+}
+
+window.addEventListener('load', function(){
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+
+});
