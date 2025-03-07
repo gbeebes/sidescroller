f6cd04d8380bb4c0c1a16d13feb6356f639ae709
Gillian Beebe <gillian@Mac.lan>
2025-03-06 20:13:05 -0500

    changing colours

--- main.js
+++ main.js
@@ -5,10 +5,18 @@ class Game {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
     }
+    render(){
+        this.ctx.fillRect(100, 100, 50, 150);
+    }
 }
 
-window.addEventListener('load', function(){
-    const canvas = this.document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
 
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getCsontext('2d');
+    canvas.width = 720;
+    canvas.height = 720;
+
+    const game = new Game(canvas, ctx);
+    game.render();
 });
